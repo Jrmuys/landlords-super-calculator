de1a5f93dcf0cce00d6d2eb13e9f1a565dd5943e
Joel Muyskens <jrmuyskens@gmail.com>
2023-02-27 17:40:49 -0500

    Feat: Initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'concrete-calculator';
+  calculator = new FormGroup({
+    type: new FormControl('concrete', Validators.required),
+    volume: new FormControl(250, Validators.required),
+  });
+
+
+
+  result: { volume: number, cement: number, sand: number, aggregate: number, water: number } = {
+    volume: 0,
+    cement: 0,
+    sand: 0,
+    aggregate: 0,
+    water: 0
+  };
+
+  constructor() {
+    this.calculator.valueChanges.subscribe(() => {
+      this.calculate();
+    });
+    this.calculate();
+
+  }
+
+  calculate() {
+    const { type, volume } = this.calculator.value;
+    if (!volume || volume <= 0) return;
+    if (type === "concrete") {
+      this.result = {
+        volume,
+        aggregate: volume * .5,
+        cement: volume * .15,
+        sand: volume * 0.25,
+        water: volume * 0.1
+      }
+    } else if (type === "mortar") {
+      this.result = {
+        volume,
+        aggregate: volume * 0,
+        cement: volume * .15,
+        sand: volume * 0.75,
+        water: volume * 0.1
+      }
+    } else {
+      this.result = {
+        volume,
+        aggregate: volume * 150 / 250,
+        cement: volume * 0.1,
+        sand: volume * 0,
+        water: volume * 75 / 250
+      }
+    }
+  }
+
+  onSubmit() {
+    console.log(this.calculator.value);
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDdDcgLzu7OspH5cJ9GMThxR5WYvr384Zg",
+  authDomain: "landlord-super-calculator.firebaseapp.com",
+  projectId: "landlord-super-calculator",
+  storageBucket: "landlord-super-calculator.appspot.com",
+  messagingSenderId: "1008914820893",
+  appId: "1:1008914820893:web:afb96c85390505ecccfbf4",
+  measurementId: "G-8GKQVZXDXF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
